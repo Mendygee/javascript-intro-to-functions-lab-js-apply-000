a0660d6a427fa1088061f693ed92f1b39b5294e2
Menachem Goorevitch <menachemgoorevitch@gmail.com>
2017-10-10 02:54:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,14 +18,18 @@ function sayHiToGrandma(string) {
 
   var lowerCase = 'hello';
   var upperCase = 'HELLO';
-  var mixedCase = 'Hi there!'
+  var mixedCase = 'Hi there!';
+  
 if (string == lowerCase) {
-  return "I can't hear you!"
+  return "I can't hear you!";
 }
+
 if (string == upperCase) {
-  return "YES INDEED!"
+  return "YES INDEED!";
 }
+
 if (string == "I love you, Grandma.") {
-  return "I love you, too."
+  return "I love you, too.";
 }
+
 }
